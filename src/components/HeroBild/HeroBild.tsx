f1d03f7e5b60a8b65a2e1137b9ab1e91c6759dbe
SerhiiBabych <sergeiy.babich@gmail.com>
2025-12-29 19:11:46 +0100

    Initial commit

--- src/components/HeroBild/HeroBild.tsx
+++ src/components/HeroBild/HeroBild.tsx
@@ -1,11 +1,11 @@
 import { useState, useEffect, useRef } from "react";
-import { motion } from "framer-motion"; 
-import spaghettiImg from "../../assets/HeroBild/spaghetti.png"; 
-import teppichImg from "../../assets/HeroBild/teppich.jpg"; 
-import carrotImg from "../../assets/HeroBild/carrot.jpg"; 
-import nutsImg from "../../assets/HeroBild/nuts.jpg"; 
-import appleImg from "../../assets/HeroBild/apple.jpg"; 
-import "./HeroBild.css"; 
+import { motion } from "framer-motion";
+import spaghettiImg from "../../assets/HeroBild/spaghetti.png";
+import teppichImg from "../../assets/HeroBild/teppich.jpg";
+import carrotImg from "../../assets/HeroBild/carrot.jpg";
+import nutsImg from "../../assets/HeroBild/nuts.jpg";
+import kiLogoImg from "../../assets/HeroBild/ki-logo.png";
+import "./HeroBild.css";
 
 type Product = {
   name: string;
@@ -15,17 +15,17 @@ type Product = {
 const productsList: Product[] = [
   { name: "teppich", img: teppichImg },
   { name: "spaghetti", img: spaghettiImg },
-  { name: "Морковь", img: carrotImg },
-  { name: "Орехи", img: nutsImg },
-  { name: "Яблоко", img: appleImg },
+  /*{ name: "Морковь", img: carrotImg },
+  { name: "Орехи", img: nutsImg },*/
+  { name: "Яблоко", img: kiLogoImg },
 ];
 
 const finalPositions = [
   { x: 150, y: 100 },
   { x: 170, y: 120 },
-  { x: 250, y: 400 },
-  { x: 350, y: 400 },
-  { x: 450, y: 400 },
+  { x: 150, y: 100 },
+  { x: 150, y: 400 },
+  { x: 150, y: 400 },
 ];
 
 export default function HeroBild() {
@@ -36,7 +36,7 @@ export default function HeroBild() {
     productsList.forEach((p, index) => {
       const timeout = window.setTimeout(() => {
         setFlyingProducts((prev) => [...prev, p]);
-      }, index * 500);
+      }, index * 100);
       timeouts.current.push(timeout);
     });
 
@@ -55,15 +55,23 @@ export default function HeroBild() {
           className={`product ${product.name}`}
           initial={{
             x: Math.random() * 300,
-            y: -100,
+            y: -120,
+            opacity: 0,
             scale: 1,
           }}
           animate={{
             x: finalPositions[i]?.x || 0,
             y: finalPositions[i]?.y || 0,
+            opacity: 1,
             scale: 1,
           }}
-          transition={{ duration: 1, ease: "easeInOut" }}
+          transition={{
+            duration: 0.5,
+            /*ease: [0.4, 0, 0.2, 1],*/
+            type: "spring",
+            stiffness: 50,
+            damping: 8,
+          }}
         />
       ))}
     </div>
